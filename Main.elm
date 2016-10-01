eaedb3b6f6d05cab99760a8a6e3e76d351fb3215
Thomas Ballinger <thomasballinger@gmail.com>
2016-10-01 14:38:44 -0400

    Simple dragging

---
+++ Main.elm
@@ -0,0 +1,166 @@
+module Main exposing (..)
+
+import AnimationFrame
+import Html.App as App
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Mouse exposing (Position)
+import Time
+import Html.Events exposing (onMouseDown)
+import Json.Decode as Json exposing ((:=))
+
+
+main : Program Never
+main =
+    App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions =
+            \model ->
+                case model.drag of
+                    --Nothing -> AnimationFrame.times Tick
+                    Nothing ->
+                        Sub.none
+
+                    Just drag ->
+                        Sub.batch
+                            [ Mouse.moves (DragAt drag.clipId)
+                            , Mouse.ups (DragEnd drag.clipId)
+                            ]
+        }
+
+
+type
+    Msg
+    --    = Tick Time.Time
+    = DragStart Int Position
+    | DragAt Int Position
+    | DragEnd Int Position
+
+
+init =
+    ( { clips = [ (Clip 0 "The star spangled banner" 0 120 10) ]
+      , drag = Nothing
+      }
+    , Cmd.none
+    )
+
+
+type alias Model =
+    { clips : List Clip
+    , drag : Maybe Drag
+    }
+
+
+type alias Clip =
+    { id : Int
+    , source : String
+    , sourceStart : Float
+    , length : Float
+    , start : Float
+    }
+
+
+type alias Drag =
+    { clipId : Int
+    , start : Position
+    , current : Position
+    }
+
+
+applyDrag : Model -> Model
+applyDrag model =
+    case model.drag of
+        Just drag ->
+            let
+                newClips =
+                    List.map
+                        (\clip ->
+                            if clip.id == drag.clipId then
+                                { clip | start = clip.start + (toFloat drag.current.x) - (toFloat drag.start.x) }
+                            else
+                                clip
+                        )
+                        model.clips
+            in
+                { model
+                    | clips = newClips
+                    , drag = Nothing
+                }
+
+        Nothing ->
+            model
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( (updateHelp msg model), Cmd.none )
+
+
+updateHelp : Msg -> Model -> Model
+updateHelp msg ({ drag } as model) =
+    case msg of
+        DragStart id xy ->
+            { model | drag = Just (Drag id xy xy) }
+
+        DragAt id xy ->
+            { model | drag = (Maybe.map (\{ start, clipId } -> Drag clipId start xy) model.drag) }
+
+        --TODO this update should be based on the current zoom
+        DragEnd id _ ->
+            applyDrag model
+
+
+
+-- View
+
+
+clipView : Clip -> Svg Msg
+clipView clip =
+    rect
+        [ fill "#7FD13B"
+        , x (toString clip.start)
+        , y "107.392"
+        , width (toString clip.length)
+        , height "10"
+        , Html.Events.on "mousedown" (Json.map (DragStart clip.id) Mouse.position)
+        ]
+        []
+
+
+view model =
+    svg
+        [ version "1.1"
+        , x "0"
+        , y "0"
+        , viewBox "0 0 200 200"
+        ]
+        (List.map clipView
+            model.clips
+        )
+
+
+nop =
+    svg
+        [ version "1.1"
+        , x "0"
+        , y "0"
+        , viewBox "0 0 323.141 322.95"
+        ]
+        [ polygon [ fill "#F0AD00", points "161.649,152.782 231.514,82.916 91.783,82.916" ] []
+        , polygon [ fill "#7FD13B", points "8.867,0 79.241,70.375 232.213,70.375 161.838,0" ] []
+        , rect
+            [ fill "#7FD13B"
+            , x "192.99"
+            , y "107.392"
+            , width "107.676"
+            , height "108.167"
+            , transform "matrix(0.7071 0.7071 -0.7071 0.7071 186.4727 -127.2386)"
+            ]
+            []
+        , polygon [ fill "#60B5CC", points "323.298,143.724 323.298,0 179.573,0" ] []
+        , polygon [ fill "#5A6378", points "152.781,161.649 0,8.868 0,314.432" ] []
+        , polygon [ fill "#F0AD00", points "255.522,246.655 323.298,314.432 323.298,178.879" ] []
+        , polygon [ fill "#60B5CC", points "161.649,170.517 8.869,323.298 314.43,323.298" ] []
+        ]
